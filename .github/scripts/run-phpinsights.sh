4a5be2bbaf5a09247321efa6c2e799f4c621d699
Kennedy Tedesco <kennedyt.tw@gmail.com>
2019-10-14 20:29:52 -0300

    actions

---
+++ .github/scripts/run-phpinsights.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+./vendor/bin/phpinsights --no-interaction --min-quality=90 --min-complexity=90 --min-architecture=90 --min-style=90
---
+++ .github/scripts/run-phpstan.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+./vendor/bin/phpstan analyse src tests --level 7
